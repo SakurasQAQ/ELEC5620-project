fb85203d77f9be41dfdd197803317b0703f40db4
jihyun-joslyn <129327085+jihyun-joslyn@users.noreply.github.com>
2024-10-11 12:42:55 +1100

    project skeleton

---
+++ learningassistant/src/main/java/com/elec5620/learningassistant/LearningassistantApplication.java
@@ -0,0 +1,13 @@
+package com.elec5620.learningassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningassistantApplication.class, args);
+	}
+
+}
